eda0afd89e7a74faa9ce215b671a31a580ebe47c
Brandon <digihew@gmail.com>
2017-08-20 17:57:09 -0400

    Setup Register form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,10 +3,39 @@ import React, { Component } from 'react';
 
 
 class Register extends Component{
+
+    handleInputChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+      
+    handleRegister = (e) => {
+        e.preventDefault();
+        let keys = Object.keys(this.state);
+        // Do axios call here
+    } 
+
     render(){
+        
         return(
             <div>
-
+                <form action="/register" method='POST'>
+                    <input type="text" name='firstName' placeholder='First Name'
+                    onClick={(e)=>this.handleInputChange(e)}/>
+                    <input type="text" name='lastName' placeholder='Last Name'
+                    onClick={(e)=>this.handleInputChange(e)}/>
+                    <input type="text" name='userName' placeholder='User Name'
+                    onClick={(e)=>this.handleInputChange(e)}/>
+                    <input type="email" name='email' placeholder='Email'
+                    onClick={(e)=>this.handleInputChange(e)}/>
+                    <input type="password" name='password' placeholder='Password'
+                    onClick={(e)=>this.handleInputChange(e)}/>
+                    <input onClick={(e)=>this.handleRegister(e)} type="submit" value='Submit'/>
+                </form>
             </div>    
         )
     }
