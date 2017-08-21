8e7eaa592a6dcae3bfb743e65302afbc07acf86e
Daniel Beebe <danwbeebe@gmail.com>
2017-08-21 11:50:40 -0400

    add route for pick quiz type to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Login from './components/Login';
 import Main from './components/Main';
 import NameDeck from './components/NameDeck';
 import CreateCard from './components/CreateCard';
+import PickQuizType from './components/PickQuizType';
 
 import axios from 'axios';
 
@@ -69,6 +70,7 @@ class App extends Component {
             <Route exact path="/main" component={Main} />
             <Route exact path="/namedeck" component={NameDeck} />
             <Route exact path="/createcard" component={CreateCard} />
+            <Route exact path="/pickquiztype" component={PickQuizType} />
           </div>
           <Footer />
         </div>
