9b8085de1b2480c8230ea63aab64ec15da87b225
Brandon Hew <digihew@gmail.com>
2017-08-21 11:45:43 -0400

    Verify server recieves data from frontend post (test)

--- client/src/App.js
+++ client/src/App.js
@@ -33,13 +33,14 @@ class App extends Component {
     e.preventDefault();
     axios.post('/auth/register', {
       options
-    }).then(res => {
+    })
+    .then(res => {
+      this.setState(options)
       this.setState({
-        auth: res.data.auth,
-        user: res.data.user,
         currentPage: 'home',
       });
-    }).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
   }
 
 
@@ -60,9 +61,9 @@ class App extends Component {
         <div className="App">
           <Header />
           <div className="main">
-            <Route exact path="/" component={Welcome} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" render={() => <Welcome />} />
+            <Route exact path="/register" render={() => <Register handleRegisterSubmit={this.handleRegisterSubmit} />} />
+            <Route exact path="/login" render={() => <Login handleLoginSubmit={this.handleLoginSubmit} />} />
           </div>
           <Footer />
         </div>
