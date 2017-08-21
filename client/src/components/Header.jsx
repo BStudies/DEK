96781ca33c3cd0fb7243439b41f9cd34d4494142
Daniel Beebe <danwbeebe@gmail.com>
2017-08-21 11:58:45 -0400

    add nav code to header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,10 @@ class Header extends Component {
     return (
       <div className='header'>
         <h1>DEK</h1>
+        <div className='nav'>
+          <button>My Account</button>
+          <button>Logout</button>
+        </div>
       </div>
     )
   }
