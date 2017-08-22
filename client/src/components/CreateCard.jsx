ec85366fb215340a29b3dcc84ead05747b01badf
ivan alvarez <vitarka88@gmail.com>
2017-08-22 16:39:16 -0400

    minor changes to createcard

--- client/src/components/CreateCard.jsx
+++ client/src/components/CreateCard.jsx
@@ -50,45 +50,44 @@ class CreateCard extends Component{
             <div className="create-front-side-card">
 
               <form method="POST" onSubmit={this.handleFormSubmit}>
-
-                  <input
+                <input
                   type="text"
                   placeholder="Question"
                   name="question"
                   value={this.state.question}
                   onChange={this.handleInputChange}
-                  />
+                />
 
                 <input
-                className="save-front-side-card"
-                type="submit"
-                value="SUBMIT QUESTION"
+                  className="save-front-side-card"
+                  type="submit"
+                  value="SUBMIT QUESTION"
                 />
               </form>
 
             </div>
           </div>
-          {/* <div className="create-back">
+          <div className="create-back">
             <div className="create-back-side-card">
 
               <form method="POST" onSubmit={this.handleFormSubmit}>
                 <input
-                type="text"
-                placeholder="Answer"
-                name="answer"
-                value={this.state.answer}
-                onChange={this.handleInputChange}
+                  type="text"
+                  placeholder="Answer"
+                  name="answer"
+                  value={this.state.answer}
+                  onChange={this.handleInputChange}
                 />
 
                 <input
-                className="save-back-side-card"
-                type="submit"
-                value="SUBMIT ANSWER"
+                  className="save-back-side-card"
+                  type="submit"
+                  value="SUBMIT ANSWER"
                 />
               </form>
 
             </div>
-          </div> */}
+          </div>
 
         <Footer />
       </div>
