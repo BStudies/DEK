53d5c59e7a879959e97ed408a3359872f169e993
Brandon Hew <digihew@gmail.com>
2017-08-23 14:55:06 -0400

    setup card componenet for edit to use

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 class Card extends Component{
     constructor(){
         super();
+        this.state = {
+            didMount: false,
+        }
     }
     componentDidMount(){
         this.setState({
@@ -13,40 +16,57 @@ class Card extends Component{
             setTime: this.props.card.setTime, 
             timesRight: this.props.card.timesRight, 
             timesWrong: this.props.card.timesWrong, 
-            deckNumber: this.props.card.deckNumber
+            deckNumber: this.props.card.deckNumber,
+            didMount: true,
         })
     }
+
+
+
+    handleInputChange = (e) => {
+        const name=e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]:value,
+        })
+    }
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log('this is where we should put to /decks')
+    }
+
+
     render(){
+        if(this.state.didMount){
+            return(
+                <div className="cards-container">
+                    <div className="create-front">
+                        <div className="create-front-side-card">
+                            <p>Edit your question</p>
+                            <form action="/decks" method='POST'>
+                                <textarea onChange={e=>this.handleInputChange(e)} name="question" cols="30" rows="10" value={this.state.question}></textarea>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="create-back">
+                        <div className="create-back-side-card">
+                            <p>Edit your answer</p>
+                            <form action="/decks" method='POST'>
+                                <textarea onChange={e=>this.handleInputChange(e)} name="answer" cols="30" rows="10" value={this.state.answer}></textarea>
+                                <input onClick={e=>{this.handleFormSubmit(e)}}type="submit" class='save-back-side-card' value='save'/>
+                            </form>
+                        </div>
+                    </div>
+
+                    
+                </div>
+                
+            )
+        }
         return(
-            <div className="card">
-                <div className="edit-front-card">
-                <div className="show-front">
-                <p>Front of card to be edited... </p>
-                </div>
-
-                <div className="edit-delete">
-                <button className="edit-card-button">Edit</button>
-                <button className="delete-card-button">Delete</button>
-                </div>
-            </div>
-
-            <div className="edit-back-card">
-
-                <div className="show-back">
-                <p>Back of card to be edited...</p>
-                </div>
-
-                <div className="next-card-arrow">
-                <p>Arrow goes here</p>
-                </div>
-
-                <div className="edit-delete">
-                <button className="edit-card">Edit</button>
-                <button className="delete-card">Delete</button>
-                </div>
-
-            </div>
-            </div>
+            <div>
+                Loading...
+            </div>    
         )
         
     }
